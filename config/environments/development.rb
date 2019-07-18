ae45bf9ca72d4f32ee32e6466242c98de44b100b
Haris Tanveer <haristanveer.ht@gmail.com>
2019-07-18 13:35:31 +0500

    heroku try

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://asli-imdb.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
