5a882d4915cfc00212f9086966944fe614b36534
Haris Tanveer <haristanveer.ht@gmail.com>
2019-07-22 13:02:30 +0500

    routes edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
-      port: 587,
+      port: 465,
       user_name: 'crickethowzat7@gmail.com',
       password: 'howzathowzat',
       authentication: 'plain',
